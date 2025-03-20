c34e774e1ee094ef1d63ad810052938a4e56cc46
Ankit_Kumar <kumarankit512@gmail.com>
2025-03-19 22:43:09 -0400

    LC002 - Optimized Solution

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,22 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-        n = len(nums)
-        for i in range(n - 1):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []
+        # n = len(nums)
+        # for i in range(n - 1):
+        #     for j in range(i + 1, n):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        # return []
 
 # Time Complexity -> O(n^2) -> Brute Force
 # Space Complexity -> O(1)
+
+        mp = {}
+        for i, n in enumerate(nums):
+            complement = target - n
+            if complement in mp:
+                return [mp[complement], i]
+            mp[n] = i
+
+# Time Complexity: O(n) -> Hash Map
+# Space Complexity: O(n)
