44960efab1bada09d37a08d83c42a147a26e677d
Ankit_Kumar <kumarankit512@gmail.com>
2025-03-19 22:40:29 -0400

    LC005

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s:str, i, j, pali):
+        while i >= 0 and j < len(s) and s[i] == s[j]:
+            i = i - 1
+            j = j + 1
+        i = i + 1
+        j = j - 1   
+        if (j - i + 1) > pali[1]:
+            pali[0] = i
+            pali[1] = j - i + 1
+
+    def longestPalindrome(self, s: str) -> str:
+        pali = [0, 0] #Start, length
+        for i in range(len(s)):
+            self.isPalindrome(s, i, i, pali)
+            self.isPalindrome(s, i, i + 1, pali)
+        return s[pali[0]: pali[0] + pali[1]]
+
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)	
