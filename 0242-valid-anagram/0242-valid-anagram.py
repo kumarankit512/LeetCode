715e515fbd2ade20e631a5ea98ae3c22b80a1888
Ankit_Kumar <kumarankit512@gmail.com>
2025-06-03 17:33:54 -0400

    Optimized O(n) solution

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,9 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-        if len(s) != len(t):
-            return False
-        return sorted(s) == sorted(t)
+        # if len(s) != len(t):
+        #     return False
+        # return sorted(s) == sorted(t)
 
         # Time Complexity: O(n log n)
-        # Space Complexity: O(n) 
+        # Space Complexity: O(n)
+        
+        if len(s) != len(t):
+            return False
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
+
+        # Time Complexity: O(n)
+        # Space Complexity: O(n)  
