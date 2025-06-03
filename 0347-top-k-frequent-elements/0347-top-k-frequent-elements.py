e6dcab75942b5955467b1b7d625dff8a5605474d
Ankit_Kumar <kumarankit512@gmail.com>
2025-06-03 17:55:45 -0400

    LC 0347

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,12 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = defaultdict(int)
+        for n in nums:
+                freq[n] += 1
+        sorted_items = sorted(freq.items(), key = lambda x: x[1], reverse = True)
+        top_k = [i[0] for i in sorted_items[:k]] 
+        
+        return top_k
+
+        #Time Complexity: O(n log n)
+        #Space Complexity: O(n)
