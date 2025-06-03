7534e3da763fe34e2fe3e77d8ab4ba4f3f5e482f
Ankit_Kumar <kumarankit512@gmail.com>
2025-06-03 18:16:51 -0400

    Optimized O(n) Solution

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,12 +1,27 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = defaultdict(int)
-        for n in nums:
-                freq[n] += 1
-        sorted_items = sorted(freq.items(), key = lambda x: x[1], reverse = True)
-        top_k = [i[0] for i in sorted_items[:k]] 
+        # freq = defaultdict(int)
+        # for n in nums:
+        #         freq[n] += 1
+        # sorted_items = sorted(freq.items(), key = lambda x: x[1], reverse = True)
+        # top_k = [i[0] for i in sorted_items[:k]] 
         
-        return top_k
+        # return top_k
 
         #Time Complexity: O(n log n)
+        #Space Complexity: O(n)
+
+        counter = defaultdict(int)
+        for n in nums:
+            counter[n] += 1
+        heap = []
+        for key, val in counter.items():
+            heapq.heappush(heap, (-val, key))
+        
+        res = []
+        while len(res) < k:
+            res.append(heapq.heappop(heap)[1])
+        return res
+
+	    #Time Complexity: O(n log n)
         #Space Complexity: O(n)
