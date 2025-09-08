cbae2f1f1f99d5563675722c871f142d1e88f3ea
Ankit Kumar <97005360+kumarankit512@users.noreply.github.com>
2025-09-07 23:39:54 -0400

    Time: 1483 ms (5.07%), Space: 21 MB (39.6%) - LeetHub

---
+++ 1039-find-the-town-judge/1039-find-the-town-judge.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+
+        '''
+        Conditions:
+        1) Each a should have a corresponding b - Satisfies the 2nd condition
+        2) b cannot have a corresponding a - Satisfies the 1st condition 
+            - Return -1 if it does
+        '''
+
+        for judge in range(1, n + 1):
+            trusting = True
+            trusted_upon = 0
+
+            for a, b in trust:
+                if a == judge:
+                    trusting = False
+                if b == judge:
+                    trusted_upon += 1
+
+            if trusting and trusted_upon == n - 1:
+                return judge
+
+        return -1
+
+#Time Complexity:
+#Space Complexity:
