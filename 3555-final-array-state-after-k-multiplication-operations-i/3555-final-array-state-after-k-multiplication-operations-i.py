8a40d89f2ce91419b985acd0c9bbaf216d5c59c2
Ankit Kumar <97005360+kumarankit512@users.noreply.github.com>
2025-09-09 23:53:47 -0400

    Time: 0 ms (100%), Space: 17.9 MB (29%) - LeetHub

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.py
@@ -0,0 +1,35 @@
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        '''
+        Given:
+        1) Array 'nums
+        2) integer 'k'
+        3) integer 'multiplier'
+
+        Given the array 'nums' we have to perform an operation 'k' number of times. We have to take the minimum value 'x' in the array and multiply it with the 'multiplier' however if a value appears twice in the array we take the value that appears first. In the end we return the final array. 
+
+        Pseudo:
+        1. Heapify the given array list
+        2. Loop through the array to find the minimum value 'x'
+        3. Mutiply the minimum value 'x' with the 'multiplier'
+        5. Perform an inplace operation to replace the minimum value 'x' with x * multiplier
+        6. return the array 
+        '''
+
+        # heap = [(num, i) for i, num in enumerate(nums)]
+        # heapify(heap)
+
+        heap = []
+        for index in range(len(nums)):
+            heap.append((nums[index], index))
+        heapify(heap)
+        
+        while k > 0:
+            min_val, index = heappop(heap)
+            temp = min_val * multiplier
+            #Update the value of array 'nums' at the current index with temp
+            nums[index] = temp
+            heappush(heap, (temp, index))
+            k -= 1
+        return nums
+
