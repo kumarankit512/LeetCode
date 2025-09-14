9a0f5e6e21f8d33950be8197f947b993dc912c85
Ankit Kumar <97005360+kumarankit512@users.noreply.github.com>
2025-09-14 19:39:09 -0400

    Time: 0 ms (100%), Space: 18 MB (59.97%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.py
@@ -0,0 +1,24 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        '''
+        Procedure:
+        - Use a stack to keep track of valid scores.
+        - Iterate through each operation:
+            - If operation is an integer -> push it into the stack.
+            - If "+" -> push the sum of the last two scores from the stack.
+            - If "D" -> push double the last score from the stack.
+            - If "C" -> pop the last score from the stack.
+        - At the end, sum all values in the stack and return it.
+        '''
+
+        stack = []
+        for op in operations:
+            if op == "+":
+                stack.append(stack[-1] + stack[-2])
+            elif op == "D":
+                stack.append(2 * stack[-1])
+            elif op == "C":
+                stack.pop()
+            else:
+                stack.append(int(op))
+        return sum(stack)
