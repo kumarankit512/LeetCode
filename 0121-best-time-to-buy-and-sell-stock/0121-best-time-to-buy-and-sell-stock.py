d76cb4a59e0aa5b2fee9e8cd1b04f55bc3038b04
Ankit Kumar <97005360+kumarankit512@users.noreply.github.com>
2025-09-19 15:00:54 -0400

    Time: 97 ms (47.9%), Space: 27 MB (8.09%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # result = 0
+        # for i in range(len(prices)):
+        #     buy = prices[i]
+        #     for j in range(i+1, len(prices)):
+        #         sell = prices[j]
+        #         result = max(result, sell - buy)
+        # return result
+
+# Brute Force: TLE
+#Time Complexity: O(n^2)
+#Space Complexity: O(1)
+
+        l = 0 
+        r = 1
+        maxProfit = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxProfit = max(maxProfit, profit)
+            else:
+                l = r
+            r += 1
+        return maxProfit
+
+#Time Complexity: O(n)
+#Space Complexity: O(1)
+
