f7042506fe025c91d597d8d41b418cc4c9c5fb3c
Ankit Kumar <97005360+kumarankit512@users.noreply.github.com>
2025-09-27 17:11:07 -0400

    Time: 8 ms (23.7%), Space: 20.8 MB (84.08%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -6,13 +6,33 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.diameter = 0
+        '''
+        Procedure:
+        1) Define a helper function that computes the height (max depth) of a subtree
+            - Height of a node = 1 + max(left height, right height)
+        2) While computing height, also check the longest path that goes through that node
+            - Path length = left height + right height
+        3) Keep track of the maximum diameter seen so far in a variable
+        4) Return result after DFS traversal finishes
+        '''
+
+        self.maxDiameter = 0
+
         def dfs(node):
             if not node:
                 return 0
-            left_height = dfs(node.left)
-            right_height = dfs(node.right)
-            self.diameter = max(self.diameter, left_height + right_height)
-            return 1 + max(left_height, right_height)
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            # Update diameter
+            self.maxDiameter = max(self.maxDiameter, left + right)
+
+            # Return height of subtree
+            return 1 + max(left, right)
+
         dfs(root)
-        return self.diameter
+        return self.maxDiameter
+
+#Time Complexity: O(n)
+#Space Complexity: O(h)
