d437b5606ac66bc66386d2cb46c5793fcdd10a8f
Ankit Kumar <97005360+kumarankit512@users.noreply.github.com>
2025-10-05 02:04:23 -0400

    Time: 93 ms (76.54%), Space: 28.4 MB (68.01%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,5 +1,21 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        
+        '''
+        Procedure:
+        1) Initialize two pointers
+        2) Compute the are at each step:
+            - The area is determined by the shorter line between height[l] and height[r], multiplied by the distance (r - l)
+        3) Update maximum area:
+            - Compare the current area with the stored maximum (res) and keep the larger one
+        4) Move pointers strategically:
+            - If height[l] <= height[r], increment l (because moving the shorter line may give a taller line, possibly increasing the area)
+            - Else, decrement r
+        5) Repeat until pointers meet:
+            - Stop when l >= r
+        6) Return the maximum area found
+        '''
+
         l = 0
         r = len(height) - 1
         res = 0
@@ -11,6 +27,3 @@ class Solution:
             else:
                 r -= 1
         return res
-
-#Time Complexity: O(n)
-#Space Complexity: O(1)
