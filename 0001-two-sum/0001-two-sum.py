9af02d69b1ec2ec8d97646c11cff55fa97d54349
Ankit Kumar <97005360+kumarankit512@users.noreply.github.com>
2025-10-06 01:55:48 -0400

    Time: 1 ms (57.78%), Space: 19 MB (23.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -9,8 +9,26 @@ class Solution:
         # return []
 
 # Time Complexity -> O(n^2) -> Brute Force
-# Space Complexity -> O(1)
 
+        # A = []
+        # for i, num in enumerate(nums):
+        #     A.append([num, i])
+        
+        # A.sort()
+        # i, j = 0, len(nums)- 1
+        # while i < j:
+        #     sum = A[i][0] + A[j][0]
+        #     if sum == target:
+        #         return [min(A[i][1], A[j][1]), max(A[i][1], A[j][1])]
+        #     elif sum < target:
+        #         i += 1
+        #     else:
+        #         j -= 1
+        # return []
+
+# Time Complexity -> O(n log (n)) -> Sorting
+# Space Complexity: O(n)
+    
         mp = {}
         for i, n in enumerate(nums):
             complement = target - n
@@ -18,5 +36,6 @@ class Solution:
                 return [mp[complement], i]
             mp[n] = i
 
-#Time Complexity: O(n) -> Hash Map
-#Space Complexity: O(n)
+#Hash Map Solution:
+# Time Complexity: O(n) 
+# Space Complexity: O(n)
