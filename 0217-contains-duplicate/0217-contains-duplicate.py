8101addb3afd38d51b5e862ee86fa880e974e484
Ankit Kumar <97005360+kumarankit512@users.noreply.github.com>
2025-12-10 20:43:15 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,30 +1,48 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # for i in range(len(nums)):
-        #     for j in range(i + 1, len(nums)):
-        #         if nums[i] == nums[j]: 
-        #             return True
-        # return False
-
-#Brute Force -> TLE
-#Time Complexity: O (n^2) 
-#Space Complexity: O(1)
-
-        # nums.sort()
-        # for i in range(1, len(nums)):
-        #     if nums[i] == nums[i-1]:
-        #         return True
-        # return False
-
-#Time Complexity: O(n log n) -> because of sorting
-#Space Complexity: O(1)
-
-        nSet = set()
-        for n in nums:
-            if n in nSet:
-                return True
-            nSet.add(n)
-        return False
         
-#Time Complexity: O(n)
-#Space Complexity: O(n)
+        '''
+        Given:
+        1) Integer array 'nums'
+        2) return true if an element appears twice
+        3) return false if every element is distinct
+
+        Brute Force:
+        Procedure:
+        1) Initialize a variable n for len(nums)
+        2) Use two while loops one starts at index 0 and the other at 1
+        3) Compare the values at both index
+        4) Return true if value matches
+        5) Return false if value is different
+        '''
+
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] == nums[j]: 
+                    return True
+        return False
+
+        #Time Complexity: O(n^2) 
+        #Space Complexity: O(1)
+
+        '''
+        Sorting Approach:
+        Procedure:
+        1) Sort the array
+        2) Loop through the array
+        3) Compare the values at the current index and prev index
+        4) Return true if value matches
+        5) Return false if value is different
+        '''
+
+        n = len(nums)
+        nums.sort()
+        for i in range(1, n):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False
+
+        #Time Complexity: O(n log n)
+        #Space Complexity: O(1)
