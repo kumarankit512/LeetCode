bdfba3319fd017a15a7ddec900b947bdf5b1048a
Ankit Kumar <97005360+kumarankit512@users.noreply.github.com>
2025-12-10 20:46:10 -0500

    Time: 49 ms (7.97%), Space: 26.6 MB (97.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -16,13 +16,13 @@ class Solution:
         5) Return false if value is different
         '''
 
-        n = len(nums)
+        # n = len(nums)
 
-        for i in range(n):
-            for j in range(i + 1, n):
-                if nums[i] == nums[j]: 
-                    return True
-        return False
+        # for i in range(n):
+        #     for j in range(i + 1, n):
+        #         if nums[i] == nums[j]: 
+        #             return True
+        # return False
 
         #Time Complexity: O(n^2) 
         #Space Complexity: O(1)
