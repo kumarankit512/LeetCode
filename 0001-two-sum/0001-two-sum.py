160c2529ef553149d9d0177086c33d326bc10de5
Ankit Kumar <97005360+kumarankit512@users.noreply.github.com>
2025-12-10 20:02:10 -0500

    Time: 1671 ms (29.09%), Space: 18.6 MB (72.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,41 +1,29 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-        # n = len(nums)
-        # for i in range(n - 1):
-        #     for j in range(i + 1, n):
-        #         if nums[i] + nums[j] == target:
-        #             return [i, j]
-        # return []
+        '''
+        Brute Force Method:
+            Given:
+            1) Array of integers 'nums' and 'target'
+            2) Return the indices of the two numbers that add up to 'target'
+            3) Can return the answer in any order
 
-# Time Complexity -> O(n^2) -> Brute Force
+            Procedure:
+            1) Initialize a variable for the length of nums
+            2) Use two while loops 
+                -> for i in range(n) -> Starts at index 0
+                    -> for j in range(i + 1, n) -> Starts at index 1
+            3) Check if the value at i and j add up to the 'target'
+            4) If we find a pair, return the indices 
+        '''
 
-        # A = []
-        # for i, num in enumerate(nums):
-        #     A.append([num, i])
+        n = len(nums)
         
-        # A.sort()
-        # i, j = 0, len(nums)- 1
-        # while i < j:
-        #     sum = A[i][0] + A[j][0]
-        #     if sum == target:
-        #         return [min(A[i][1], A[j][1]), max(A[i][1], A[j][1])]
-        #     elif sum < target:
-        #         i += 1
-        #     else:
-        #         j -= 1
-        # return []
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
 
-# Time Complexity -> O(n log (n)) -> Sorting
-# Space Complexity: O(n)
-    
-        mp = {}
-        for i, n in enumerate(nums):
-            complement = target - n
-            if complement in mp:
-                return [mp[complement], i]
-            mp[n] = i
-
-#Hash Map Solution:
-# Time Complexity: O(n) 
-# Space Complexity: O(n)
+        #Time Complexity -> O(n^2)
+        #Space Complexity -> O(1)
