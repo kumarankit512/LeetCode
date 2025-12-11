de0471d92a85cf28adf12d6d9a7175ca451fe0bc
Ankit Kumar <97005360+kumarankit512@users.noreply.github.com>
2025-12-10 20:25:40 -0500

    Time: 19 ms (14.88%), Space: 18.5 MB (18.78%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,20 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-
-        # if len(s) != len(t):
-        #     return False
-        # return sorted(s) == sorted(t)
-
-        # Time Complexity: O(n log n)
-        # Space Complexity: O(n)
         
+        '''
+        Given:
+        1) Two strings 's' and 't'
+        2) return true if t is an anagram of s, else false
+
+        Sorting Approach:
+        Procedure:
+        1) Firstly check if the length of both the strings are same
+        2) Return false if the lengths are different
+        3) Return sorted(s) == sorted(t)
+        '''
+
         if len(s) != len(t):
             return False
-        countS, countT = {}, {}
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        return countS == countT
+        return sorted(s) == sorted(t)
 
-        # Time Complexity: O(n)
-        # Space Complexity: O(n)  
+        #Time Complexity: O(n log n)
+        #Space Complexity: O(n)
