e1e83268053ad567c4053face2e12cf256737ac9
Ankit Kumar <97005360+kumarankit512@users.noreply.github.com>
2026-01-01 13:43:27 -0500

    Time: 0 ms (100%), Space: 17.3 MB (86.09%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -3,33 +3,29 @@ class Solution:
         
         '''
         Procedure:
-        1) Start from the last digit
-            - Because when you add 1, it might cause a carry only near the end
+        1) Iterate backwards, starting from the last digit
         2) Add 1 to the last digit
         3) If the sum is less than 10
             - Update the digit and return the array
         4) If the sum is 10 or greater
             - Set the current digit to 0
             - Carry 1 to the next more significant digit
-        5) Continue this process moving leftward through the array
-        6) If you reach the beginning of the array and still have a carry (For Example: [9,9,9] -> [0,0,0] with carry 1)
+        5) If you reach the beginning of the array and still have a carry (For Example: [9,9,9] -> [0,0,0] with carry 1)
             - Insert 1 at the beginning of the array
-        7) Return the updated array
+        6) Return the updated array
         '''
 
         n = len(digits)
 
-        # tart from the last digit
         for i in range(n - 1, -1, -1):
             digits[i] += 1 
 
             if digits[i] < 10:
-                return digits   #No carry, return result
+                return digits
 
-            digits[i] = 0       #Carry over to the next digit
+            digits[i] = 0
 
-        #If all digits were 9, add 1 at the beginning
         return [1] + digits
 
-#Time Complexity: O(n)
-#Space Complexity: O(1)
+        #Time Complexity: O(n)
+        #Space Complexity: O(n)
